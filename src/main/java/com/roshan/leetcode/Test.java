4700ea4ec5f5d25066e97cb3ef07a296af52fd3d
wwn <wwn@mankebao.com>
2019-12-17 16:56:08 +0800

    add 29

--- src/main/java/com/roshan/leetcode/Test.java
+++ src/main/java/com/roshan/leetcode/Test.java
@@ -4,7 +4,13 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
-        int[] a = new int[0];
-        System.out.println(a);
+//        System.out.println("1: " + Integer.toHexString(1));
+//        System.out.println("0: " + Integer.toHexString(0));
+//        System.out.println("-0: " + Integer.toHexString(-0));
+//        System.out.println("-1: " + Integer.toHexString(-1));
+//        System.out.println("-2: " + Integer.toHexString(-2));
+//        System.out.println("Integer.MIN_VALUE: " + Integer.toHexString(Integer.MIN_VALUE));
+//        System.out.println("Integer.MAX_VALUE: " + Integer.toHexString(Integer.MAX_VALUE));
+        System.out.println(-1 >> 1);
     }
 }
