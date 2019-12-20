cde6056624c42f62c577822685444b4ccd8e7cf8
wwn <wwn@mankebao.com>
2019-12-20 16:30:45 +0800

    add 34

--- src/main/java/com/roshan/leetcode/Test.java
+++ src/main/java/com/roshan/leetcode/Test.java
@@ -1,25 +1,6 @@
 package com.roshan.leetcode;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {3,2,1};
-        Arrays.sort(arr,1,3);
-        System.out.print("321: ");
-        for (int i : arr) {
-            System.out.print(i);
-        }
-        System.out.println();
-        System.out.print("321: ");
-        int[] ar = {3,2,1};
-        Arrays.sort(ar);
-        for (int i : ar) {
-            System.out.print(i);
-        }
-        System.out.println();
     }
 }
--- src/main/java/com/roshan/leetcode/Test0034_FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/com/roshan/leetcode/Test0034_FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,40 +1,43 @@
 package com.roshan.leetcode;
 
-import org.junit.Test;
-
 import java.util.Arrays;
 
 public class Test0034_FindFirstAndLastPositionOfElementInSortedArray {
-    @Test
-    public void test() {
-        int[] nums = new int[]{0,1,2,3,4,5,6,6};
-        int target = 6;
-        int[] ret = searchRange(nums, target);
-        System.out.println("result:[" + ret[0] + "," + ret[1] + "]");
+
+
+    private static int[] mySolution(int[] nums, int target) {
+        return new int[]{binarySearch(nums, target, true), binarySearch(nums, target, false)};
     }
 
-    public int[] searchRange(int[] nums, int target) {
-        int length = nums.length;
-        if (length == 0) {
-            return new int[]{-1, -1};
-        }
-        if (length == 1) {
-            if (nums[0] == target) {
-                return new int[]{0, 0};
+    private static int binarySearch(int[] nums, int target, boolean isLeft) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
             } else {
-                return new int[]{-1, -1};
+                if (isLeft) {
+                    if (mid == 0 || nums[mid - 1] != target) {
+                        return mid;
+                    } else {
+                        right = mid - 1;
+                    }
+                } else {
+                    if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                        return mid;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
             }
         }
-        int middle = length / 2;
-        if (nums[middle] < target) {
-            int[] larger = Arrays.copyOfRange(nums, middle, length);
-            return searchRange(larger, target);
-        } else if (nums[middle] > target) {
-            int[] smaller = Arrays.copyOf(nums, middle);
-            return searchRange(smaller, target);
-        } else if (nums[middle] == target) {
-            return new int[]{666, 666};
-        }
-        return new int[0];
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,3};
+        System.out.println(Arrays.toString(mySolution(nums, 1)));
     }
 }
