c325b2d490db26eb3591740eda397ae56b2cc6ac
wwn <wwn@mankebao.com>
2019-12-24 15:37:37 +0800

    add 38

---
+++ src/main/java/com/roshan/leetcode/Test0038_CountAndSay.java
@@ -0,0 +1,29 @@
+package com.roshan.leetcode;
+
+import java.util.Stack;
+
+public class Test0038_CountAndSay {
+    public static String mySolution(int n) {
+        String str = "1";
+        for (int i = 1; i < n; i++) {
+            char[] chars = str.toCharArray();
+            int start = 0, end = 0;
+            StringBuilder temp = new StringBuilder();
+            while (end < chars.length) {
+                int count = 0;
+                while (end < chars.length && chars[end] == (chars[start])) {
+                    end++;
+                    count++;
+                }
+                temp.append(count).append(chars[start]);
+                start = end;
+            }
+            str = temp.toString();
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySolution(5));
+    }
+}
