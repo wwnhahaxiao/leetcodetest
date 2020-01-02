01bf661abb0354435a25acac8e29707355e5018c
wwn <wwn@mankebao.com>
2020-01-02 09:37:45 +0800

    add 43

--- src/main/java/com/roshan/leetcode/Test0043_MultiplyStrings.java
+++ src/main/java/com/roshan/leetcode/Test0043_MultiplyStrings.java
@@ -1,14 +1,21 @@
 package com.roshan.leetcode;
 
-import sun.misc.BASE64Encoder;
+import java.math.BigDecimal;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Base64;
-
-public class Test {
-
-    public static String multiply(String num1, String num2) {
+//Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+//        Example 1:
+//        Input: num1 = "2", num2 = "3"
+//        Output: "6"
+//        Example 2:
+//        Input: num1 = "123", num2 = "456"
+//        Output: "56088"
+//        Note:
+//        The length of both num1 and num2 is < 110.
+//        Both num1 and num2 contain only digits 0-9.
+//        Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+//        You must not use any built-in BigInteger library or convert the inputs to integer directly.
+public class Test0043_MultiplyStrings {
+    private static String mySolution(String num1, String num2) {
         if (num1.equals("0") || num2.equals("0")) {
             return "0";
         }
@@ -39,6 +46,7 @@ public class Test {
     public static void main(String[] args) {
         String num1 = "123";
         String num2 = "456";
-        multiply(num1, num2);
+        //56088
+        System.out.println(mySolution(num1, num2));
     }
 }
