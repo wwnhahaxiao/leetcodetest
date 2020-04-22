e4b6ba40a36b2d766c977ece75d863aaac664437
wwn <wwnhahaxiao@gmail.com>
2020-04-23 00:11:24 +0800

    add 97

--- src/main/java/com/roshan/leetcode/Test0097_InterleavingString.java
+++ src/main/java/com/roshan/leetcode/Test0097_InterleavingString.java
@@ -1,4 +1,7 @@
 package com.roshan.leetcode;
+
+import java.util.Arrays;
+
 //Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
 //        Example 1:
 //        Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
@@ -7,7 +10,44 @@ package com.roshan.leetcode;
 //        Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
 //        Output: false
 public class Test0097_InterleavingString {
-    private static boolean isInterleave(String s1, String s2, String s3) {
-        return true;
+    private static char[] c1,c2,c3;
+    private static int[][] checked;
+
+    private static boolean mySolution(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        c1 = s1.toCharArray();
+        c2 = s2.toCharArray();
+        c3 = s3.toCharArray();
+        checked = new int[s1.length() + 1][s2.length() + 1];
+        for (int[] arr : checked) {
+            Arrays.fill(arr, -1);
+        }
+        return match(0, 0, 0);
+    }
+    private static boolean match(int p1, int p2, int p3) {
+        if (p3 == c3.length) {
+            return true;
+        }
+        if (checked[p1][p2] >= 0) {
+            return checked[p1][p2] == 1;
+        }
+        if (p1 < c1.length && c3[p3] == c1[p1] && match(p1 + 1, p2, p3 + 1)) {
+            checked[p1][p2] = 1;
+            return true;
+        }
+        if (p2 < c2.length && c3[p3] == c2[p2] && match(p1, p2 + 1, p3 + 1)) {
+            checked[p1][p2] = 1;
+            return true;
+        }
+        checked[p1][p2] = 0;
+        return false;
+    }
+
+    public static void main(String[] args) {
+//        String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
+        String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc";
+        System.out.println(mySolution(s1, s2, s3));
     }
 }
