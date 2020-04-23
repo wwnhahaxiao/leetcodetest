6ba5db5e5f27b2b0228eec7e26078f3fa567ed0b
roshan <361439400@qq.com>
2020-04-23 16:28:39 +0800

    add 99

--- src/main/java/com/roshan/leetcode/Test0097_InterleavingString.java
+++ src/main/java/com/roshan/leetcode/Test0097_InterleavingString.java
@@ -50,4 +50,23 @@ public class Test0097_InterleavingString {
         String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc";
         System.out.println(mySolution(s1, s2, s3));
     }
+
+    private static boolean mySolution2(String s1, String s2, String s3) {
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i - 1);
+                } else {
+                    dp[i][j] = (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1))
+                            || (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1));
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
 }
