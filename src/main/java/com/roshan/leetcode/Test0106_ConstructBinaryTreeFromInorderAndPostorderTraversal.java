d8631437d6cbd05e06ec05b55b801c1693a21654
roshan <361439400@qq.com>
2020-04-26 15:54:49 +0800

    add 106

--- src/main/java/com/roshan/leetcode/Test0106_ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ src/main/java/com/roshan/leetcode/Test0106_ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -5,55 +5,54 @@ import com.roshan.leetcode.baseStructure.TreeNode;
 import java.util.HashMap;
 import java.util.Map;
 
-//Given preorder and inorder traversal of a tree, construct the binary tree.
+//Given inorder and postorder traversal of a tree, construct the binary tree.
 //        Note:
 //        You may assume that duplicates do not exist in the tree.
 //        For example, given
-//        preorder = [3,9,20,15,7]
-//        inorder = [9,3,15,20,7]
+//        inorder = [9,3,15,20,7]
+//        postorder = [9,15,7,20,3]
 //        Return the following binary tree:
 //          3
 //         / \
 //        9  20
 //          /  \
 //         15   7
-public class Test0105_ConstructBinaryTreeFromPreorderAndInorderTraversal {
-
-    private static int[] pre, in;
+public class Test0106_ConstructBinaryTreeFromInorderAndPostorderTraversal {
+    private static int[] in, post;
     private static Map<Integer, Integer> map;
-    private static int preIndex;
+    private static int postIndex;
 
-    private static TreeNode mySolution(int[] preorder, int[] inorder) {
-        pre = preorder;
+    private static TreeNode mySolution(int[] inorder, int[] postorder) {
         in = inorder;
+        post = postorder;
         map = new HashMap<>();
         for (int i = 0; i < in.length; i++) {
             map.put(in[i], i);
         }
-        preIndex = 0;
+        postIndex = post.length - 1;
         return buildNode(0, inorder.length - 1);
     }
 
     private static TreeNode buildNode(int start, int end) {
         if (start == end) {
-            preIndex++;
+            postIndex--;
             return new TreeNode(in[start]);
         } else if (start > end) {
             return null;
         } else {
-            int value = pre[preIndex++];
+            int value = post[postIndex--];
             TreeNode root = new TreeNode(value);
             int inIndex = map.get(value);
-            root.left = buildNode(start, inIndex - 1);
             root.right = buildNode(inIndex + 1, end);
+            root.left = buildNode(start, inIndex - 1);
             return root;
         }
     }
 
     public static void main(String[] args) {
-        int[] preOrder = {3,9,20,15,7};
         int[] inOrder = {9,3,15,20,7};
-        TreeNode treeNode = mySolution(preOrder, inOrder);
+        int[] postorder = {9,15,7,20,3};
+        TreeNode treeNode = mySolution(inOrder, postorder);
         System.out.println(treeNode);
     }
 }
