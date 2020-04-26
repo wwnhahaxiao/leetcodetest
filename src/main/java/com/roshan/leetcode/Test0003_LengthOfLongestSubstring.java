3890375725c67cf82caee67283d4576476dcefbd
roshan <361439400@qq.com>
2020-04-26 16:43:03 +0800

    update 3

--- src/main/java/com/roshan/leetcode/Test0003_LengthOfLongestSubstring.java
+++ src/main/java/com/roshan/leetcode/Test0003_LengthOfLongestSubstring.java
@@ -6,38 +6,24 @@ import java.util.Map;
 public class Test0003_LengthOfLongestSubstring {
 
     private static int mySolution(String s) {
-        Map<Character, Integer> map = new HashMap();
-        int max = 0, len = 0, n = s.length();
-        for (int i = 0; i < n; i++) {
-            if (map.containsKey(s.charAt(i))) {
-                len = Math.max(map.get(s.charAt(i)), len);
+        Map<Character, Integer> map = new HashMap<>();
+        int len = 0, left = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                len = Math.max(i - Math.max(map.get(c), left), len);
+                left = Math.max(map.get(c), left);
+            } else {
+                len = Math.max(i - left, len);
             }
-            max = Math.max(i - len + 1, max);
-            map.put(s.charAt(i), i + 1);
+            map.put(c, i);
         }
-        return max;
+        return len;
     }
 
     public static void main(String[] args) {
-        String str = "abcdasdfasdghadhifofhasdbfjkabjh";
+        String str = " ";
 //      String str = "012345678901234567890123456789";
         System.out.println(mySolution(str));
-        System.out.println(lengthOfLongestSubstring(str));
     }
-
-
-    private static int lengthOfLongestSubstring(String s) {
-        int n = s.length(), ans = 0, len = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            Character c = s.charAt(i);
-            if (map.containsKey(c)) {
-                len = Math.max(map.get(c), len);
-            }
-            ans = Math.max(ans, i - len + 1);
-            map.put(c, i + 1);
-        }
-        return ans;
-    }
-
 }
