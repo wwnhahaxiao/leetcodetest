7a17076fbbb5b8e2c898effb27b8eeca7e36c901
wwn <wwnhahaxiao@gmail.com>
2020-05-01 09:17:22 +0800

    add 112

---
+++ src/main/java/com/roshan/leetcode/Test0112_PathSum.java
@@ -0,0 +1,35 @@
+package com.roshan.leetcode;
+
+import com.roshan.leetcode.baseStructure.TreeNode;
+import org.junit.Test;
+
+//Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+//        Note: A leaf is a node with no children.
+//        Example:
+//        Given the below binary tree and sum = 22,
+//              5
+//             / \
+//            4   8
+//           /   / \
+//          11  13  4
+//         /  \      \
+//        7    2      1
+//        return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+public class Test0112_PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    @Test
+    public void test() {
+        TreeNode node = TreeNode.build(5,4,8,11,null,13,4,7,2,null,null,null,1);
+        boolean b = hasPathSum(node, 22);
+        System.out.println(b);
+    }
+}
