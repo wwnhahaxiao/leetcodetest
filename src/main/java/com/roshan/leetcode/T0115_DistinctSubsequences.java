2c349e5eb6bc4d982e752c882b4a471daf308187
wwn <wwnhahaxiao@gmail.com>
2020-05-02 22:02:31 +0800

    add 115

---
+++ src/main/java/com/roshan/leetcode/T0115_DistinctSubsequences.java
@@ -0,0 +1,29 @@
+package com.roshan.leetcode;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class T0115_DistinctSubsequences {
+    private int numDistinct(String s, String t) {
+        int[][] dp = new int[t.length() + 1][s.length() + 1];
+        Arrays.fill(dp[0], 1);
+        for (int i = 1; i <= t.length(); i++) {
+            for (int j = i; j <= s.length(); j++) {
+                if (s.charAt(j - 1) == t.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[t.length()][s.length()];
+    }
+
+    @Test
+    public void test() {
+        String s = "babgbag", t = "bag";
+        int i = numDistinct(s, t);
+        System.out.println(i);
+    }
+}
