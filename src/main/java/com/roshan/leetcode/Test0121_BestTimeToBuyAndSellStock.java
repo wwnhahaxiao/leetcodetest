66e833498c9239018bc192d07c7a65b37dc0f4e6
roshan <361439400@qq.com>
2020-05-07 16:45:19 +0800

    add 121 todo

---
+++ src/main/java/com/roshan/leetcode/Test0121_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,11 @@
+package com.roshan.leetcode;
+
+public class Test0121_BestTimeToBuyAndSellStock {
+    private int maxProfit(int[] prices) {
+        int price = Integer.MAX_VALUE;
+        for (int p : prices) {
+            //todo
+        }
+        return 0;
+    }
+}
