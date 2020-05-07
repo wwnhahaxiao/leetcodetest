843f33b7d08de388cb3566cc1be6e88b4d3613ce
roshan <361439400@qq.com>
2020-05-07 11:05:52 +0800

    add 119

--- src/main/java/com/roshan/leetcode/Test0118_PascalsTriangle.java
+++ src/main/java/com/roshan/leetcode/Test0118_PascalsTriangle.java
@@ -1,9 +1,8 @@
 package com.roshan.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.SortedSet;
+import org.junit.Test;
+
+import java.util.*;
 
 //Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
 //        In Pascal's triangle, each number is the sum of the two numbers directly above it.
@@ -18,31 +17,29 @@ import java.util.SortedSet;
 //        [1,4,6,4,1]
 //        ]
 public class Test0118_PascalsTriangle {
-    private static List<List<Integer>> mySolution(int numRows) {
+
+    private List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> first = new ArrayList<>();
+        List<Integer> currRow = new ArrayList<>();
         for (int i = 0; i < numRows; i++) {
-            first = new ArrayList<>(next(first));
-            result.add(first);
+            List<Integer> nextRow = new LinkedList<>();
+            Integer pre = 0;
+            ListIterator<Integer> iterator = currRow.listIterator();
+            while (iterator.hasNext()) {
+                Integer curr = iterator.next();
+                nextRow.add(pre + curr);
+                pre = curr;
+            }
+            nextRow.add(1);
+            result.add(nextRow);
+            currRow = nextRow;
         }
         return result;
     }
 
-    private static List<Integer> next(List<Integer> list) {
-        List<Integer> nextList = new ArrayList<>();
-        ListIterator<Integer> iterator = list.listIterator();
-        Integer temp = 0;
-        while (iterator.hasNext()) {
-            Integer next = iterator.next();
-            nextList.add(next + temp);
-            temp = next;
-        }
-        nextList.add(1);
-        return nextList;
-    }
-
-    public static void main(String[] args) {
-        List<List<Integer>> lists = mySolution(5);
-        System.out.println(lists);
+    @Test
+    public void test() {
+        List<List<Integer>> generate = generate(5);
+        System.out.println(generate);
     }
 }
