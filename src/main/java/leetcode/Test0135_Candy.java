0ad68f993990f63f8f96083199dfd1c028b7adae
wwn <wwnhahaxiao@gmail.com>
2020-06-07 17:56:26 +0800

    add 135

--- src/main/java/leetcode/Test0135_Candy.java
+++ src/main/java/leetcode/Test0135_Candy.java
@@ -3,6 +3,8 @@ package leetcode;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 //There are N children standing in a line. Each child is assigned a rating value.
 //        You are giving candies to these children subjected to the following requirements:
@@ -19,34 +21,46 @@ import java.util.Arrays;
 //        Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
 //        The third child gets 1 candy because it satisfies the above two conditions.
 public class Test0135_Candy {
-    public int candy2(int[] ratings) {
+    private int candy(int[] ratings) {
+        int candies = 1, direction = 0, up = 0, down = 0;
         for (int i = 1; i < ratings.length; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-
+            int newDirection = Integer.compare(ratings[i], ratings[i - 1]);
+            //分数相同或者分数到达最低时为一轮 计算之前所有学生的糖果
+            if (newDirection == 0 || (newDirection > 0 && newDirection != direction)) {
+                candies += sum(up) + sum(down) + Math.max(up, down);
+                up = down = 0;
             }
+            if (newDirection > 0) {
+                up++;
+            } else if (newDirection == 0) {
+                candies++;
+            } else {
+                down++;
+            }
+            direction = newDirection;
         }
-        return 0;
+        return candies + sum(up) + sum(down) + Math.max(up, down);
     }
-    public int candy(int[] ratings) {
-        int[] result = new int[ratings.length];
-        Arrays.fill(result, 1);
-        for (int i = 1; i < ratings.length; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                result[i] = result[i - 1] + 1;
-            }
-        }
-        for (int i = ratings.length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                result[i] = Math.max(result[i + 1] + 1, result[i]);
-            }
-        }
-        return Arrays.stream(result).sum();
+
+    private int sum(int n) {
+        return (n + 1) * n / 2;
     }
 
     @Test
     public void test() {
-        int[] ratings = {1,3,2,2,1};
-        int candy = candy(ratings);
-        System.out.println(candy);
+        Map<int[], Integer> testMap = new HashMap<>();
+        testMap.put(new int[]{3,2,1,2,3}, 11);
+        testMap.put(new int[]{1,0,2}, 5);
+        testMap.put(new int[]{1,2,3,1,0}, 9);
+        testMap.put(new int[]{1,6,10,8,7,3,2}, 18);
+        testMap.put(new int[]{1,3,2,2,1}, 7);
+        testMap.put(new int[]{1,3,4,5,2}, 11);
+        testMap.put(new int[]{1,2,3,1,0}, 9);
+        testMap.put(new int[]{1,2,87,87,87,2,1}, 13);
+        testMap.entrySet().stream()
+                .filter(entry -> candy(entry.getKey()) != entry.getValue())
+                .findFirst()
+                .ifPresent(entry -> System.out.println(Arrays.toString(entry.getKey()) + ":" + entry.getValue()));
     }
+
 }
