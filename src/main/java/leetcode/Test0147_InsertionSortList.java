3437ec2f890f2839ca8645c1440db17f8531eb46
Roshan <361439400@qq.com>
2020-06-19 17:05:08 +0800

    add temp

--- src/main/java/leetcode/Test0147_InsertionSortList.java
+++ src/main/java/leetcode/Test0147_InsertionSortList.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import leetcode.baseStructure.ListNode;
+import org.junit.Test;
 
 //Sort a linked list using insertion sort.
 //        A graphical example of insertion sort. The partial sorted list (black) initially contains only the first element in the list.
@@ -16,11 +17,83 @@ import leetcode.baseStructure.ListNode;
 //        Input: -1->5->3->4->0
 //        Output: -1->0->3->4->5
 public class Test0147_InsertionSortList {
-    private ListNode insertionSortList(ListNode head) {
-        while (head != null && head.next != null) {
-            ListNode next = head.next;
+    public ListNode insertionSortList1(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        dummy.next = head;
+        ListNode pre = head;
+        ListNode curr = head.next;
+        while (curr != null) {
+            ListNode next = curr.next;
 
+            if (curr.val < pre.val) {
+                pre.next = null;
+                curr.next = null;
+                correct(dummy, curr);
+            }
+            pre = curr;
+            curr = next;
         }
-        return null;
+    }
+    public ListNode insertionSortList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode pre = head;
+        ListNode cur = head.next;
+        dummy.next = head;
+        while (cur != null) {
+            if (pre.val <= cur.val) {// 本来就有序
+                pre = cur;
+                cur = cur.next;
+            } else {
+                ListNode p = dummy;
+                // 找到一个位置使得p < cur < p.next
+                while (p.next != cur && p.next.val < cur.val) {
+                    p = p.next;
+                }
+                // 将cur插入到p和p.next之间
+                // 因为cur被插到前面，pre的指针需要跳过cur
+                pre.next = cur.next;
+                cur.next = p.next;
+                p.next = cur;
+                // 完成插入后，cur回到pre后面
+                cur = pre.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    private ListNode insertionSortList(ListNode head) {
+        ListNode correct = new ListNode(Integer.MIN_VALUE);
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = null;
+            correct(correct, head);
+            head = next;
+        }
+        return correct.next;
+    }
+
+    private void correct(ListNode head, ListNode node) {
+        ListNode pre = null;
+        while (head != null) {
+            if (head.val > node.val) {
+                pre.next = node;
+                node.next = head;
+                return;
+            }
+            pre = head;
+            head = head.next;
+        }
+        pre.next = node;
+    }
+
+    @Test
+    public void test() {
+        ListNode head = ListNode.build(4,2,1,3);
+        ListNode listNode = insertionSortList(head);
+        System.out.println(listNode);
     }
 }
