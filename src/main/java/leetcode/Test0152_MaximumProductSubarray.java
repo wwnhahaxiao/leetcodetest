aa709ab600f8a2ce2501cf28156858aa1194dfda
Roshan <361439400@qq.com>
2020-06-23 15:39:40 +0800

    add 152

--- src/main/java/leetcode/Test0152_MaximumProductSubarray.java
+++ src/main/java/leetcode/Test0152_MaximumProductSubarray.java
@@ -54,30 +54,6 @@ public class Test0152_MaximumProductSubarray {
         return Math.max(Math.max(leftMax, rightMax), midMax);
     }
 
-
-    private static int mySolution(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int max = nums[0], min = nums[0], result = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            int temp = max;
-            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
-            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
-            result = Math.max(max, result);
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        int[] a = new int[]{2,3,-2,4};
-        int[] b = new int[]{-2,0,-1};
-        int[] c = new int[]{-2,0,1,2,3,0,-2,1,-2,2,3,-2,-2};
-        System.out.println(mySolution(a));
-        System.out.println(mySolution(b));
-        System.out.println(mySolution(c));
-    }
-
     @Test
     public void test() {
         int[] a = new int[]{2,3,-2,4};
