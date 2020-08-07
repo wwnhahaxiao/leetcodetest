54b67f8534a99b0dc98da06731864e82979a0631
roshan <wwnhahaxiao@gmail.com>
2020-08-07 16:15:11 +0800

    add 187

--- src/main/java/leetcode/Test0187_RepeatedDNASequences.java
+++ src/main/java/leetcode/Test0187_RepeatedDNASequences.java
@@ -16,8 +16,8 @@ public class Test0187_RepeatedDNASequences {
 
     private List<String> findRepeatedDnaSequences(String s) {
         Set<String> set = new HashSet<>();
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < s.length() - 10; i++) {
+        Set<String> result = new HashSet<>();
+        for (int i = 0; i <= s.length() - 10; i++) {
             String sub = s.substring(i, i + 10);
             if (set.contains(sub)) {
                 result.add(sub);
@@ -25,12 +25,13 @@ public class Test0187_RepeatedDNASequences {
                 set.add(sub);
             }
         }
-        return result;
+        return new ArrayList<>(result);
     }
 
     @Test
     public void test() {
-        String dnaSeq = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+//        String dnaSeq = "AAAAAAAAAAA";
+        String dnaSeq = "AAAAAAAAAAAA";
         List<String> repeatedDnaSequences = findRepeatedDnaSequences(dnaSeq);
         System.out.println(repeatedDnaSequences);
     }
