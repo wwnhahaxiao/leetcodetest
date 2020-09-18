7f60d6341611a78c030cbf32d0b216ce7666bb67
roshan <wwnhahaxiao@gmail.com>
2020-09-18 14:23:36 +0800

    add 188

--- src/main/java/leetcode/Test0188_BestTimeToBuyAndSellStockIV.java
+++ src/main/java/leetcode/Test0188_BestTimeToBuyAndSellStockIV.java
@@ -21,55 +21,42 @@ import java.util.Map;
 //        Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
 //                     Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
 public class Test0188_BestTimeToBuyAndSellStockIV {
-    private int maxProfit2(int k, int[] prices) {
-        int[] mostProfit = new int[k];
-        int start = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < prices[i - 1]) {
-                int profit = prices[i - 1] - prices[start];
-                insert(mostProfit, profit);
-            }
-        }
-        return Arrays.stream(mostProfit).sum();
-    }
 
-    private void insert(int[] profits, int profit) {
-        for (int i = 0; i < profits.length; i++) {
-            if (profits[i] > profit) {
-                profits[i] = profit;
-            } else {
-                profits[i] = profits[i + 1];
-            }
-        }
-    }
     private int maxProfit(int k, int[] prices) {
-        int[] money = new int[2 * k];
-        Arrays.fill(money, Integer.MIN_VALUE);
+        if (k == 0 || prices.length == 0) return 0;
+        //当k过大时转化为无限次交易
+        if (k >= prices.length / 2) {
+            int buy = -prices[0], sold = 0;
+            for (int price : prices) {
+                buy = Math.max(sold - price, buy);
+                sold = Math.max(buy + price, sold);
+            }
+            return sold;
+        }
+        //int[i][j]代表第i次交易j=0买入j=1卖出后剩余钱数
+        int[][] profits = new int[k][2];
         for (int i = 0; i < prices.length; i++) {
             int price = prices[i];
-            for (int j = 0; j < money.length && j <= i; j++) {
-                if (j % 2 == 0) {
-                    int pre = j == 0 ? 0 : money[j - 1];
-                    money[j] = Math.max(pre - price, money[j]);
+            for (int j = 0; j < k; j++) {
+                if (i == 0) {
+                    profits[j][0] = -price;
                 } else {
-                    money[j] = Math.max(money[j - 1] + price, money[j]);
+                    profits[j][0] = Math.max((j > 0 ? profits[j - 1][1] : 0) - price, profits[j][0]);
+                    profits[j][1] = Math.max(profits[j][0] + price, profits[j][1]);
                 }
             }
         }
-        int result = 0;
-        for (int profit : money) {
-            result = Math.max(profit, result);
-        }
-        return result;
+        return profits[k - 1][1];
     }
 
     @Test
     public void test() {
         Map<Pair<int[], Integer>, Integer> testMap = new HashMap<>();
+        testMap.put(new Pair<>(new int[]{1,3}, 0), 0);
         testMap.put(new Pair<>(new int[]{2,4,1}, 2), 2);
-//        testMap.put(new Pair<>(new int[]{3,2,6,5,0,3}, 2), 7);
+        testMap.put(new Pair<>(new int[]{3,2,6,5,0,3}, 2), 7);
         testMap.entrySet().stream()
-                .filter(entry -> maxProfit2(entry.getKey().getValue(), entry.getKey().getKey()) != entry.getValue())
+                .filter(entry -> maxProfit(entry.getKey().getValue(), entry.getKey().getKey()) != entry.getValue())
                 .findAny()
                 .ifPresent(entry -> System.out.println(Arrays.toString(entry.getKey().getKey())));
     }
